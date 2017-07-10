674782bc26318745bca92a56b3f9d703da6fb000
Sourav Mandal <sourav2406@gmail.com>
2017-07-10 18:04:04 +0530

    Deck using doubly linked list

---
+++ linkedList/deck.py
@@ -0,0 +1,29 @@
+from dublyLinkedList import _DoublyLinkedBase
+
+class Deck(_DoublyLinkedBase):
+	def first(self):
+		if self.is_empty():
+			raise Empty('Deck is empty')
+		return self._header._next._element
+
+	def last(self):
+		if self.is_empty():
+			raise Empty('Deck is empty')
+		return self._trailer._prev._element
+
+	def insert_first(self,e):
+		self._insertBetween(e,self._header,self._header._next)
+
+	def insert_last(self,e):
+		self._insertBetween(e,self._trailer._prev,self._trailer)
+
+	def delete_first(self):
+		if self.is_empty():
+			raise Empty('Deck is empty')
+		return self._deleteNode(self._header._next)
+
+	def delete_last(self):
+		if self.is_empty():
+			raise Empty('Deck is empty')
+		return self._deleteNode(self._trailer._prev)
+		
